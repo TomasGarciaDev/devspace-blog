959d2d5b9fa6e36b0fb4340d106993f0864e13d4
Tomas Garcia <tomasgarciadev@gmail.com>
2022-07-25 14:08:18 +0200

    Post component created

---
+++ components/Post.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Post({post}) {
+  return (
+    <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">Post</div>
+  )
+}
