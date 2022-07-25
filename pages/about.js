0d27e4ccb63ac5dbee2e6fd26683d42cdf2ff67c
Tomas Garcia <tomasgarciadev@gmail.com>
2022-07-25 13:56:44 +0200

    Accesing posts as a prop using gray-matter

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,9 @@ export default function AboutPage() {
       </h1>
 
       <div className="bg-white shadow-md rounded-lg px-10 py-6 mt-6">
-        <h3 className="text-2xl mb-5"> DevSpace Blog</h3>
+        <h3 className="text-2xl mb-5">
+          DevSpace Blog
+        </h3>
 
         <p className="mb-3">This is a blog built with Next.js and Markdown</p>
 
